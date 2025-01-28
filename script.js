f26454e16e4bc08e362ab2cd2a9088ab52124052
Chris Hubley <chrisjhubley@gmail.com>
2025-01-28 13:52:26 +0000

    Added error message to form

--- script.js
+++ script.js
@@ -51,3 +51,27 @@ fetch("./portfolio.json")
     
     })    
 })
+
+
+//contact form
+document.querySelector('.form').addEventListener('submit',handleSubmit)
+
+function handleSubmit(e){
+    console.log('Form is submitting')
+
+    const nameInput = document.querySelector('#name').value
+    const emailInput = document.querySelector('#email').value
+    const commentsInput = document.querySelector('#comments').value
+
+    if (
+        nameInput.length === 0 || 
+        emailInput.length === 0 ||
+        commentsInput.length === 0)
+        {
+        document.querySelector('.error').classList.add('visible')
+        e.preventDefault()
+        console.log("Form Error")
+    
+   }
+     
+    }
